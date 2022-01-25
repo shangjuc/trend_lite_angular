7b70d88b4eae0317c0e7312ce28b93aa2a458f51
shangjuc <shangjuc@gmail.com>
2022-01-25 15:28:20 +0800

    Add tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  prefix: '',
+  purge: {
+    content: [
+      './src/**/*.{html,ts}',
+    ]
+  },
+  darkMode: 'class',
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+}
